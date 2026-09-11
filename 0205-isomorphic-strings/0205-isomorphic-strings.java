a7e2bc2fdd5278403a81718a54dfcdcb63ed920f
Himanshu chahar <himanshuchahar6396@gmail.com>
2026-09-11 12:03:51 +0530

    Time: 5 ms (98.69%), Space: 43.8 MB (58.4%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,47 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        int[] a = new int[256];
+        int[] b = new int[256];
 
-        HashMap<Character,Character>map= new HashMap<>();
-        HashMap<Character,Character>map1= new HashMap<>();
-
-       char arr[]= s.toCharArray();
-       char arr1[]=t.toCharArray();
-
-       for(int i=0;i<arr.length;i++){
-        
-      
-         if(map.containsKey(arr[i])){
-            if(map.get(arr[i])!=arr1[i]){
+        for(int i=0;i<s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(a[ch1]!=b[ch2]){
                 return false;
             }
-         }else{
-            map.put(arr[i],arr1[i]);
-         }
-         
-
-       }
-         for(int i=0;i<arr.length;i++){
-        
-      
-         if(map1.containsKey(arr1[i])){
-            if(map1.get(arr1[i])!=arr[i]){
-                return false;
-            }
-         }else{
-            map1.put(arr1[i],arr[i]);
-         }
-         
-
-       }
-     
-
-        // for(int i=0;i<arr.length;i++){
-        //     if(map.isEmpty()&& map1.isEmpty()){
-        //         return  true;
-        //     }
-        // }
-        
+            a[ch1]=i+1;
+            b[ch2]=i+1;
+        }
         return true;
-
     }
 }
