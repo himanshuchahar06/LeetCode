454344ed04f8f7aa3fdb92d6486a5dd62a6c93e9
Himanshu chahar <himanshuchahar6396@gmail.com>
2026-09-14 15:14:46 +0530

    Time: 8 ms (5.33%), Space: 52.6 MB (5.33%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        String ans = "";
+
+        for(int i = 0; i < chars.length; i++){
+
+            int count = 1;
+
+            while(i + 1 < chars.length && chars[i] == chars[i + 1]){
+                count++;
+                i++;
+            }
+
+            ans = ans + chars[i];
+
+            if(count > 1){
+                ans = ans + count;
+            }
+        }
+
+       
+        for(int i = 0; i < ans.length(); i++){
+            chars[i] = ans.charAt(i);
+        }
+
+        return ans.length();
+    }
+}
